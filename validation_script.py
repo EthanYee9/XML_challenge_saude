8e459cba2a634dc2a0191b82276326780065f7ee
EthanYee9 <ethanyee9@gmail.com>
2025-08-18 13:11:07 +0100

    adding try and except block for non well formed XML samples, adding base path, function, print statements

--- validation_script.py
+++ validation_script.py
@@ -1,19 +1,32 @@
 from lxml import etree
 from io import BytesIO
+import os 
 
-# Editing schema in memory 
-with open("Schemas/FSA029-Schema.xsd", "rb") as file:
-    schema_doc = file.read()
-    altered_schema = schema_doc.replace(b"../../CommonTypes/v14/CommonTypes-Schema.xsd", b"/home/ethan-yee/projects/XML_challange_saude/Schemas/CommonTypes-Schema.xsd")
+def FSA029_validation(sample_path, schema_folder):
+    base_path = os.path.abspath(schema_folder)
 
-altered_schema_doc = BytesIO(altered_schema)
+    # Editing schema in memory 
+    with open(f"{schema_folder}/FSA029-Schema.xsd", "rb") as file:
+        schema_doc = file.read()
+        altered_schema = schema_doc.replace(b"../../CommonTypes/v14/CommonTypes-Schema.xsd", b"CommonTypes-Schema.xsd")
 
-# Parsing schema 
-schema_doc = etree.parse(altered_schema_doc)
-xmlschema = etree.XMLSchema(schema_doc)
+    altered_schema_doc = BytesIO(altered_schema)
 
-# Parsing sample
-sample =  etree.parse("Samples/FSA029-Sample-Valid.xml")
+    # Parsing schema 
+    schema_doc = etree.parse(altered_schema_doc, base_url=f"file://{base_path}/")
+    xmlschema = etree.XMLSchema(schema_doc)
 
-# Validating  
-print(xmlschema.validate(sample))
+    # Parsing sample
+    try:
+        sample =  etree.parse(sample_path)
+    except:
+        raise Exception("XML file is not well-formed")
+
+    # Validating   
+    if xmlschema.validate(sample):
+        print("Submission has been correctly validated against FSA029 schema")
+    else:
+        print("Submission has failed validation against FSA029 schema") 
+
+if __name__ == "__main__":
+    FSA029_validation("Samples/FSA029-Sample-Valid.xml", "Schemas")

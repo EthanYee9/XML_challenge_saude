19d1410fd48e2a63e7e8a42f7b949c458fb2c84a
EthanYee9 <ethanyee9@gmail.com>
2025-08-18 12:15:47 +0100

    adding code which alters schema in memory to absolute path

--- validation_script.py
+++ validation_script.py
@@ -1,7 +1,15 @@
 from lxml import etree
+from io import BytesIO
+
+# Editing schema in memory 
+with open("Schemas/FSA029-Schema.xsd", "rb") as file:
+    schema_doc = file.read()
+    altered_schema = schema_doc.replace(b"../../CommonTypes/v14/CommonTypes-Schema.xsd", b"/home/ethan-yee/projects/XML_challange_saude/Schemas/CommonTypes-Schema.xsd")
+
+altered_schema_doc = BytesIO(altered_schema)
 
 # Parsing schema 
-schema_doc = etree.parse("Schemas/FSA029-Schema.xsd")
+schema_doc = etree.parse(altered_schema_doc)
 xmlschema = etree.XMLSchema(schema_doc)
 
 # Parsing sample
